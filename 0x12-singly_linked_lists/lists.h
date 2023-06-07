ad6762ae52665a3be6b5507c20656868868ec5c0
InspiredbyEmerald <magbayizah@gmail.com>
2023-06-07 13:05:36 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,27 +5,27 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for project
  */
-typedef struct listint_s
+typedef struct list_s
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	char*str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void free_list2(list_t **head);
-int pop_list(list_t **head);
-list_t *get_node_at_index(list_t *head, unsigned int index);
 
 #endif
+
 
