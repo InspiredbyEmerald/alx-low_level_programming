7360e56b6bf31b615c82c4b84526c46ee7d8a09f
InspiredbyEmerald <magbayizah@gmail.com>
2023-06-07 16:11:04 -0700

    eli

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,12 +12,13 @@ int pop_listint(listint_t **head)
 listint_t *temp;
 int num;
 
-from(!head || !*head)
+if (!head || !*head)
 return (0);
 
 num = (*head)->n;
 temp = (*head)->next;
-free(*head);*head = temp;
+free(*head);
+*head = temp;
 
 return (num);
 }
